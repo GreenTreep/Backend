32566b0362f9730705d5ed5c7484e43046ef567b
COSTES SYLVAIN <sylvain.costes@audioptic.fr>
2024-12-21 18:38:16 +0100

    QuickFix

--- app/src/main/java/fr/parisnanterre/greentrip/backend/entity/User.java
+++ app/src/main/java/fr/parisnanterre/greentrip/backend/entity/User.java
@@ -31,6 +31,15 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    public User(Long id, String firstName, String lastName, String email, String password, Role role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
     public User(String firstName, String lastName, String email, String password, Role role) {
         this.firstName = firstName;
         this.lastName = lastName;
